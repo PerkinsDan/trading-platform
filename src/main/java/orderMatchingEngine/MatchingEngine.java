6d6c698ca2265392a1552b22916f158caa032b34
asad-yezdan <yezdanasad@gmail.com>
2025-03-02 21:50:01 +0000

    barebones functionality for Matching engine, NOT tested

--- src/main/java/orderMatchingEngine/MatchingEngine.java
+++ src/main/java/orderMatchingEngine/MatchingEngine.java
@@ -15,6 +15,7 @@ public class MatchingEngine implements Runnable{
 
     @Override
     public void run(){
+
         while(true){
             try{
                 Order order = orderQueue.take();
@@ -32,26 +33,84 @@ public class MatchingEngine implements Runnable{
     private void MatchOrder(Order order){
         switch (order.getType()){
             case BUY:
-                processBuy(order);
+                BuyAtMarketBest(order);
             case SELL:
-                processSell(order);
+                SellAtMarketBest(order);
             default:
                 break;
         }
     }
 
-    private void processBuy(Order order){
-        //fill n put: fill an order as much as possible, and put the remaining on the books
-
-        
-
+    private void BuyAtMarketBest(Order order){
+        int buyAmount = order.getQuantity();
+        Order topOrder = sellBook.peek();
+    
+        if(topOrder == null || order.getPrice() < topPrice){
+            // sellbook was empty, add this order to the book and move on
+            // OR the buy price was just too low and no one is interested in selling at this price, add this order to the book and move on
+            sellBook.offer();
+            break;
+        }
+        //topOrder is not null and prices overlap so proceed to try matching
+        Double topPrice = topOrder.getPrice();
+        int topQuantity = topOrder.getQuantity();
+        while (buyAmount != 0){
+            if(buyAmount >= topQuantity){
+                //matched but not enough quantity to fill, fill as much as possible and move on
+                System.out.println(topOrder.getQuantity() + " lots bought at " +  topPrice);
+                buyAmount = buyAmount - topQuantity;
+                Order filledOrder = sellBook.poll();
+                System.out.println("Order with id = " + filledOrder.getId() + "has been filled and removed from the books");
+                //set new best price and quantity
+                topPrice = newBestPrice();
+                topQuantity = newBestQuantity();
+            } else {
+                System.out.println(buyAmount + "lots bought at " + topPrice );
+                topOrder.setQuantity(topQuantity - buyAmount);
+                buyAmount = 0; //set to zero to break out of loop as we've filled the order
+            }         
+        }
     }
 
-    private void processSell(Order order){
-        //fill n put: fill an order as much as possible, and put the remaining on the books
-
+    private void SellAtMarketBest(Order order){
+        //sell at market best
+        int sellAmount = order.getQuantity();
+        Order topOrder = buyBook.peek();
+    
+        if(topOrder == null || order.getPrice() > topPrice){
+            // buybook was empty, add this order to the book and move on
+            // OR the sell price was just too high and no one is interested in buying at this price, add this order to the book and move on
+            buyBook.offer();
+            break;
+        }
+        //topOrder is not null and prices overlap so proceed to try matching
+        Double topPrice = topOrder.getPrice();
+        int topQuantity = topOrder.getQuantity();
+        while (sellAmount != 0){
+            if(sellAmount >= topQuantity){
+                //matched but not enough quantity to fill, fill as much as possible adust for whats left and move on
+                System.out.println(topOrder.getQuantity() + " lots sold at " +  topPrice);
+                sellAmount = sellAmount - topQuantity;
+                Order filledOrder = sellBook.poll();
+                System.out.println("Order with id = " + filledOrder.getId() + "has been filled and removed from the books");
+                //set new best price and quantity
+                topPrice = newBestPrice();
+                topQuantity = newBestQuantity();
+            } else {
+                //matched and enough quantity to fill  
+                System.out.println(sellAmount + "lots sold at " + topPrice );
+                topOrder.setQuantity(topQuantity - sellAmount);
+                sellAmount = 0; //set to zero to break out of loop as we've filled the order
+            }         
+        }
     }
-            
+       
+    private Double newBestPrice(){
+        return sellBook.peek().getPrice();
+    }
+    private int newBestQuantity(){
+        return sellBook.peek().getQuantity();
+    }
 
     
 
