b10caabacf1e1a3ce7f42bcd99ef0161957ad9b6
Dan Perkins <danjonperkins@gmail.com>
2025-03-04 21:55:53 +0000

    fix: prettier action failing

--- apps/MatchingEngine/src/main/java/orderMatchingEngine/OrderProcessor.java
+++ apps/MatchingEngine/src/main/java/orderMatchingEngine/OrderProcessor.java
@@ -1,16 +1,15 @@
 package orderMatchingEngine;
 
-public class OrderProcessor{
+public class OrderProcessor {
 
-    public OrderProcessor(){}
+  public OrderProcessor() {}
 
-    public void addOrder(Order order){
-        TradeBook book = MatchingEngine.getTradeBook(order.getTicker());
-        book.addToBook(order);
-    }
+  public void addOrder(Order order) {
+    TradeBook book = MatchingEngine.getTradeBook(order.getTicker());
+    book.addToBook(order);
+  }
 
-    public void match(MatchingEngine matchingEngine,Order order){
-        matchingEngine.match(MatchingEngine.getTradeBook(order.getTicker()));
-    }
-
+  public void match(MatchingEngine matchingEngine, Order order) {
+    matchingEngine.match(MatchingEngine.getTradeBook(order.getTicker()));
+  }
 }
