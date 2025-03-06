98140af0cc852caca3006a2aced7a72ae030e0cb
asad-yezdan <yezdanasad@gmail.com>
2025-03-06 11:16:00 +0000

    Rewrite OrderProcessor : it holds tradebook, and matches internally, just using MatchingEngine as a library

--- apps/MatchingEngine/src/main/java/orderMatchingEngine/Main.java
+++ apps/MatchingEngine/src/main/java/orderMatchingEngine/Main.java
@@ -3,8 +3,7 @@ package orderMatchingEngine;
 public class Main {
 
   public static void main(String[] args) {
-    MatchingEngine matchingEngine = MatchingEngine.getInstance();
-    OrderProcessor processor = new OrderProcessor();
+    OrderProcessor processor = OrderProcessor.getInstance();
     // TO-DO : This temporary until we have an API to function as an entry point to the MatchingEngine
     Order[] orders = {
       new Order(OrderType.SELL, Ticker.A, 99.0, 575),
@@ -14,21 +13,21 @@ public class Main {
       new Order(OrderType.BUY, Ticker.A, 98.0, 30),
       new Order(OrderType.BUY, Ticker.A, 100.0, 700),
       new Order(OrderType.BUY, Ticker.A, 100.0, 300),
-      new Order(OrderType.BUY, Ticker.A, 102.0, 130),
+      new Order(OrderType.BUY, Ticker.A, 102.0, 130)
     };
 
     for (Order order : orders) {
       processor.addOrder(order);
     }
 
+    Order dummyOrder = new Order(OrderType.BUY, Ticker.A, 102.0, 130);
+
     //Run matching engine
     long start = System.currentTimeMillis();
-    int count = matchingEngine.match(
-      MatchingEngine.getTradeBook(Ticker.A)
-    ).size();
+    int count = processor.MatchTrades(dummyOrder).size();
     long end = System.currentTimeMillis();
     long time = end - start;
-    System.out.println(count + " trades matched in " + time + "ms");
-    System.out.println("average of " + time / 5 + "ms /trade");
+    System.out.println(count/2 + " trades matched in " + time + "ms");
+    System.out.println("average of " + (time / 5)  + "ms /trade");
   }
 }
--- apps/MatchingEngine/src/main/java/orderMatchingEngine/OrderProcessor.java
+++ apps/MatchingEngine/src/main/java/orderMatchingEngine/OrderProcessor.java
@@ -1,15 +1,55 @@
 package orderMatchingEngine;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bson.Document;
+
 public class OrderProcessor {
 
-  public OrderProcessor() {}
+  private static OrderProcessor orderProcessor = null;
+  private static final Map<Ticker, TradeBook> TradeBookMap = new HashMap<>();
+  private static final Ticker[] equities = {
+    Ticker.A,
+    Ticker.B,
+    Ticker.C,
+    Ticker.D,
+    Ticker.E,
+  };
+
+  private OrderProcessor() {
+    for (Ticker equity : equities) {
+      AddTradeBook(equity);
+    }
+  }
+
+  public static OrderProcessor getInstance(){
+    if(orderProcessor == null){
+      orderProcessor = new OrderProcessor();
+    }
+    return orderProcessor;
+  }
 
   public void addOrder(Order order) {
-    TradeBook book = MatchingEngine.getTradeBook(order.getTicker());
+    TradeBook book  = OrderProcessor.getTradeBook(order.getTicker());
     book.addToBook(order);
   }
 
-  public void match(MatchingEngine matchingEngine, Order order) {
-    matchingEngine.match(MatchingEngine.getTradeBook(order.getTicker()));
+  public ArrayList<Document> MatchTrades( Order order) {
+    return MatchingEngine.match(OrderProcessor.getTradeBook(order.getTicker()));
+  }
+
+  private void AddTradeBook(Ticker ticker) {
+    TradeBook newTradeBook = new TradeBook(ticker);
+    TradeBookMap.put(ticker, newTradeBook);
+  }
+
+  public static TradeBook getTradeBook(Ticker ticker) {
+    return TradeBookMap.get(ticker);
+  }
+  // used only for testing
+  public void resetInstance() {
+    orderProcessor = null;
   }
 }
