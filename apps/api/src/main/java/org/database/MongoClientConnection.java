0da352c5978b6206a5e1a1f9dbda96d708a69f1c
Victoria S <ec24599@qmul.ac.uk>
2025-03-06 00:40:11 +0000

    add create-order endpoint, to add to database and OrderProcessor

--- apps/api/src/main/java/org/database/MongoClientConnection.java
+++ apps/api/src/main/java/org/database/MongoClientConnection.java
@@ -7,29 +7,38 @@ import com.mongodb.ServerApi;
 import com.mongodb.ServerApiVersion;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 public class MongoClientConnection {
-    public static void main(String[] args) {
+
+    private static MongoClient mongoClient;
+    private static MongoDatabase database;
+
+    public static MongoCollection<Document> getOrdersCollection() {
+        if (mongoClient == null) {
+            initClient();
+        }
+        return database.getCollection("orders");
+    }
+
+    private static void initClient() {
         String connectionString = "mongodb+srv://databaserUser:databaseUser@trading-platform.beiiy.mongodb.net/?retryWrites=true&w=majority&appName=trading-platform";
         ServerApi serverApi = ServerApi.builder()
-                .version(ServerApiVersion.V1) //sets version to stable API version
+                .version(ServerApiVersion.V1)
                 .build();
-        MongoClientSettings settings = MongoClientSettings.builder() //builder pattern that creates configuration object for MongoDB client
+        MongoClientSettings settings = MongoClientSettings.builder()
                 .applyConnectionString(new ConnectionString(connectionString))
                 .serverApi(serverApi)
                 .build();
-        // Create a new client and connect to the server
-        try (MongoClient mongoClient = MongoClients.create(settings)) {
-            try {
-                // Send a ping to confirm a successful connection
-                MongoDatabase database = mongoClient.getDatabase("admin");
-                database.runCommand(new Document("ping", 1));
-                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
-            } catch (MongoException e) {
-                e.printStackTrace();
-            }
+        try {
+            mongoClient = MongoClients.create(settings);
+            database = mongoClient.getDatabase("tradingPlatform");
+            database.runCommand(new Document("ping", 1));
+            System.out.println("Successfully connected to MongoDB!");
+        } catch (MongoException e) {
+            e.printStackTrace();
         }
     }
 }
