8da55146c51e200a319a86baf91674a1d7db96a6
Dan Perkins <danjonperkins@gmail.com>
2025-03-06 13:42:37 +0000

    refactor OrderComparator

--- apps/MatchingEngine/src/main/java/orderMatchingEngine/OrderComparator.java
+++ apps/MatchingEngine/src/main/java/orderMatchingEngine/OrderComparator.java
@@ -4,21 +4,23 @@ import java.util.Comparator;
 
 public class OrderComparator implements Comparator<Order> {
 
-  private OrderType type;
+  private final OrderType type;
 
-  public OrderComparator(OrderType type){
+  public OrderComparator(OrderType type) {
     this.type = type;
   }
+
   @Override
   public int compare(Order order1, Order order2) {
-    if (order1.getPrice() != order2.getPrice()) {
-      if (type == OrderType.BUY) return Double.compare(
-        order2.getPrice(),
-        order1.getPrice()
-      );
-      return Double.compare(order1.getPrice(), order2.getPrice());
+    boolean samePrice = order1.getPrice() == order2.getPrice();
+
+    if (samePrice) {
+      return switch (type) {
+        case BUY -> Double.compare(order2.getPrice(), order1.getPrice());
+        case SELL -> Double.compare(order1.getPrice(), order2.getPrice());
+      };
     }
+
     return Long.compare(order1.getTimestamp(), order2.getTimestamp());
   }
-    
 }
