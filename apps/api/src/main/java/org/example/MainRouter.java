76afb0bf46722e43a4712976bace4fd075156e9a
Victoria S <ec24599@qmul.ac.uk>
2025-03-07 01:43:36 +0000

    update /create-order endpoint in line with new MatchingEngine logic

--- apps/api/src/main/java/org/example/MainRouter.java
+++ apps/api/src/main/java/org/example/MainRouter.java
@@ -1,19 +1,25 @@
 package org.example;
 
+import com.mongodb.client.model.Filters;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
-import orderMatchingEngine.*;
+import orderProcessor.Order;
+import orderProcessor.OrderProcessor;
+import orderProcessor.Ticker;
+import orderProcessor.OrderType;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.database.MongoClientConnection;
+
+import java.util.ArrayList;
 
 public class MainRouter {
 
     Router router;
-    OrderProcessor orderprocessor;
-    MatchingEngine matchingengine = MatchingEngine.getInstance();
+    OrderProcessor orderprocessor = OrderProcessor.getInstance();
 
     MainRouter(Vertx vertx) {
         router = Router.router(vertx);
@@ -31,7 +37,19 @@ public class MainRouter {
                 });
 
         router.get("/user-active-positions").handler(ctx -> {
-            // logic to retrieve user's active trade positions
+            //receive user parameters to query on
+            JsonObject body = ctx.getBodyAsJson();
+            String userID = body.getString("userID");
+            var usersCollection = MongoClientConnection.getCollection("users");
+
+            //query database for user object
+            Bson filter = Filters.gt("userID", userID);
+            usersCollection.find(filter).forEach(doc -> System.out.println(doc.toJson()));
+            //usersCollection.find(filter).forEach(doc -> );
+            //TODO: RETURN ATTRIBUTES THAT CAN BE USED TO CREATE USER OBJECT, SIMILAR TO .APPEND IN /CREATE-ORDER
+
+            //return it as user object
+
             ctx.response().end("Retrieving user's active positions...");
         });
 
@@ -57,15 +75,42 @@ public class MainRouter {
                     .append("price", price)
                     .append("quantity", quantity);
 
-            var ordersCollection = MongoClientConnection.getOrdersCollection();
+            var ordersCollection = MongoClientConnection.getCollection("orders");
             ordersCollection.insertOne(newOrderDoc);
 
             Order order = new Order(type, ticker, price, quantity);
+            ArrayList<Document> matchesFound = orderprocessor.processOrder(order);
 
-            if (orderprocessor == null) {
-                orderprocessor = new OrderProcessor();
+            if (!matchesFound.isEmpty()) {
+
+                if(matchesFound.get(0).getBoolean("filled")){
+                    ordersCollection.deleteOne(Filters.eq("orderId", matchesFound.get(0).getString("orderId")));
+                }
+
+                if(!matchesFound.get(0).getBoolean("filled")){
+                    String orderId = matchesFound.get(0).getString("orderId");
+                    int quantityChange = matchesFound.get(0).getInteger("quantityChange");
+
+                    ordersCollection.findOneAndUpdate(
+                            Filters.eq("orderId", orderId),
+                            new Document("$inc", new Document("quantity", quantityChange))
+                    );
+                }
+
+                if(matchesFound.get(1).getBoolean("filled")){
+                    ordersCollection.deleteOne(Filters.eq("orderId", matchesFound.get(0).getString("orderId")));
+                }
+
+                if(!matchesFound.get(1).getBoolean("filled")){
+                    String orderId = matchesFound.get(1).getString("orderId");
+                    int quantityChange = matchesFound.get(1).getInteger("quantityChange");
+
+                    ordersCollection.findOneAndUpdate(
+                            Filters.eq("orderId", orderId),
+                            new Document("$inc", new Document("quantity", -quantityChange))
+                    );
+                }
             }
-            orderprocessor.addOrder(order);
 
             ctx.response()
                     .setStatusCode(201)
