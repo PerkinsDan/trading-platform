5a45ca69a50976b32d0897fd65c9d208e1adc8dd
Joel Karet <joelkaret@gmail.com>
2025-03-07 14:56:02 +0000

    initial ui

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="*" element={<SignIn />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ apps/client/src/firebaseConfig/firebase.ts
@@ -0,0 +1,17 @@
+import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "tradingplatform-bc1a7.firebaseapp.com",
+  projectId: "tradingplatform-bc1a7",
+  storageBucket: "tradingplatform-bc1a7.firebasestorage.app",
+  messagingSenderId: "657364436425",
+  appId: "1:657364436425:web:7a92b5758104a7972fd418",
+  measurementId: "G-GGS7RV3PCV",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
