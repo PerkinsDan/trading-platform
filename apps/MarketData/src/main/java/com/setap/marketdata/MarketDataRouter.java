3f7259b1fa4a2ffa6c77eca2f487796c2cf11f6a
Dan Perkins <danjonperkins@gmail.com>
2025-04-08 21:22:31 +0100

    remove unused import

--- apps/MarketData/src/main/java/com/setap/marketdata/MarketDataRouter.java
+++ apps/MarketData/src/main/java/com/setap/marketdata/MarketDataRouter.java
@@ -1,7 +1,6 @@
 package com.setap.marketdata;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.Router;
