1acc8f5100e470347436811ae0e9786f96b91565
A Yezdan <yezdanasad@gmail.com>
2025-04-22 20:19:41 +0100

    POC : ValidationBuilder

---
+++ apps/api/src/main/java/com/setap/tradingplatformapi/Validations/ValidationBuilder.java
@@ -0,0 +1,60 @@
+package com.setap.tradingplatformapi.Validations;
+
+import io.vertx.core.json.JsonObject;
+import java.util.ArrayList;
+import java.util.List;
+// import whatever you need for valdiations 
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
+import com.setap.tradingplatformapi.database.MongoClientConnection;
+
+public class ValidationBuilder{
+
+    private final List<Validation> validations = new ArrayList<>();
+
+    public ValidationBuilder validateUserId(){
+        MongoCollection<Document> usersCollection =
+                            MongoClientConnection.getCollection("users");
+        validations.add(body -> 
+
+            body.containsKey("userId") &&
+            ! body.getString("userId").isBlank() &&
+            usersCollection.find(Filters.eq("userId", body.getString("userId"))).first() != null
+            
+        );
+        return this;
+    }
+
+    public ValidationBuilder validateOrderId(){
+        MongoCollection<Document> activeOrdersCollection =
+                            MongoClientConnection.getCollection("activeOrders");
+        validations.add(body -> 
+            body.containsKey("orderId") && 
+            !body.getString("orderId").isBlank() &&
+            activeOrdersCollection.find(Filters.eq("orderId", body.getString("orderId"))).first() != null
+
+        );
+        return this;
+    }
+
+    //add more methods the above two as needed, ie validateSufficientStockForSell(), validateSufficientFundsForBuy() ...
+
+    public Validation build() {
+        return body -> validations.stream().allMatch(validation -> {
+            try {
+                return validation.validate(body);
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return false;
+        });
+    }
+    
+
+}
+    
