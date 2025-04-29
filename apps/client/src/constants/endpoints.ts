e97635cb690544e885025d0437408988621f6e48
Joel Karet <joel.karet@gmail.com>
2025-04-29 21:20:55 +0100

    fixup endpoints

--- apps/client/src/constants/endpoints.ts
+++ apps/client/src/constants/endpoints.ts
@@ -2,6 +2,8 @@ const ORDER_PROCESSOR_BASE_URL = import.meta.env.VITE_APP_ORDER_PROCESSOR_BASE_U
 
 export const TRADES_PENDING_ENDPOINT = `${ORDER_PROCESSOR_BASE_URL}/users/active-positions?userId=`;
 export const TRADES_HISTORY_ENDPOINT = `${ORDER_PROCESSOR_BASE_URL}/users/trade-history?userId=`;
+export const CREATE_USER_ENDPOINT = `${ORDER_PROCESSOR_BASE_URL}/users/create`;
+export const UPDATE_USER_BALANCE_ENDPOINT = `${ORDER_PROCESSOR_BASE_URL}/users/update-user-balance`;
 export const USER_ACCOUNT_ENDPOINT = `${ORDER_PROCESSOR_BASE_URL}/users/account?userId=`;
 export const CREATE_ORDER_ENDPOINT = `${ORDER_PROCESSOR_BASE_URL}/orders/create`;
 export const CANCEL_ORDER_ENDPOINT = `${ORDER_PROCESSOR_BASE_URL}/orders/cancel`;
