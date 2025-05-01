2434c85d1df7f0ba76577d878272812e74d1b803
Dan Perkins <danjonperkins@gmail.com>
2025-05-01 23:38:51 +0100

    portfolio + use ticker enum

--- apps/client/src/components/Portfolio.tsx
+++ apps/client/src/components/Portfolio.tsx
@@ -1,12 +1,25 @@
 import { Stack, Typography } from "@mui/material";
 import { useAuth } from "../context/AuthContext";
 import { useEffect, useState } from "react";
-import { Stock } from "../../types";
+import { Snapshot, Stock, Ticker } from "../../types";
 import { USER_ACCOUNT_ENDPOINT } from "../constants/endpoints.ts";
+import useFetchSnapshots from "../hooks/useFetchSnapshots.ts";
+
+const totalValue = (portfolio: Stock[], snapshots: Map<Ticker, Snapshot>) => {
+  return portfolio.reduce((acc, stock) => {
+    const snapshot = snapshots.get(stock.ticker as Ticker);
+    if (snapshot) {
+      return acc + snapshot.price * stock.quantity;
+    }
+    return acc;
+  }, 0);
+};
 
 const Portfolio = () => {
   const currentUser = useAuth();
+  const snapshots = useFetchSnapshots();
   const [portfolio, setPortfolio] = useState<Stock[]>([]);
+  const [totalValueOfStocks, setTotalValueOfStocks] = useState("0");
 
   useEffect(() => {
     if (!currentUser) {
@@ -24,48 +37,70 @@ const Portfolio = () => {
 
       const json = await response.json();
 
-      console.log(json);
       setPortfolio(json.portfolio);
     };
 
     fetchPortfolio();
   }, [currentUser]);
 
+  useEffect(() => {
+    if (portfolio.length > 0 && snapshots) {
+      const totalValueOfStocks = totalValue(portfolio, snapshots);
+      setTotalValueOfStocks(totalValueOfStocks.toFixed(2));
+    }
+  }, [portfolio, snapshots]);
+
   return (
-    <Stack spacing="1rem">
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        sx={{ px: "0.5rem" }}
-      >
-        <Typography variant="subtitle1" fontWeight={600}>
-          Stock
-        </Typography>
-        <Typography variant="subtitle1" fontWeight={600}>
-          Quantity
-        </Typography>
-      </Stack>
-
-      {portfolio.length === 0 && (
-        <Typography color="textSecondary">No stocks owned.</Typography>
-      )}
-
-      {portfolio.map((s) => (
+    <Stack>
+      <Typography variant="h5" fontWeight={600} sx={{ mb: "1rem" }}>
+        Total Value: {"$" + totalValueOfStocks}
+      </Typography>
+      <Stack spacing="1rem">
         <Stack
-          key={s.ticker}
           direction="row"
           justifyContent="space-between"
-          sx={{
-            background: "#f5f5f5",
-            borderRadius: "0.5rem",
-            p: "0.5rem",
-            boxShadow: 1,
-          }}
+          sx={{ px: "0.5rem" }}
         >
-          <Typography>{s.ticker}</Typography>
-          <Typography>{s.quantity}</Typography>
+          <Typography variant="subtitle1" fontWeight={600}>
+            Stock
+          </Typography>
+          <Typography variant="subtitle1" fontWeight={600}>
+            Quantity
+          </Typography>
+          <Typography variant="subtitle1" fontWeight={600}>
+            Holding Value
+          </Typography>
         </Stack>
-      ))}
+
+        {portfolio.length === 0 && (
+          <Typography color="textSecondary">No stocks owned.</Typography>
+        )}
+
+        {portfolio.map((s) => {
+          const snapshot = snapshots?.get(s.ticker as Ticker); // Get the snapshot for the ticker
+          const currentValue = snapshot
+            ? (snapshot.price * s.quantity).toFixed(2)
+            : "N/A";
+
+          return (
+            <Stack
+              key={s.ticker}
+              direction="row"
+              justifyContent="space-between"
+              sx={{
+                background: "#f5f5f5",
+                borderRadius: "0.5rem",
+                p: "0.5rem",
+                boxShadow: 1,
+              }}
+            >
+              <Typography>{s.ticker}</Typography>
+              <Typography>{s.quantity}</Typography>
+              <Typography>${currentValue}</Typography>
+            </Stack>
+          );
+        })}
+      </Stack>
     </Stack>
   );
 };
--- apps/client/src/components/TradingCard.tsx
+++ apps/client/src/components/TradingCard.tsx
@@ -1,48 +1,18 @@
 import { Stack, Typography } from "@mui/material";
 import { useNavigate } from "react-router";
-import { Snapshot } from "../../types";
-import { useEffect, useState } from "react";
-import { TIME_SERIES_ENDPOINT } from "../constants/endpoints";
+import { Snapshot, Ticker } from "../../types";
 import TradeGraph from "./TradeGraph";
+import useFetchTimeSeries from "../hooks/useFetchTimeSeries";
 
 interface TradingCardProps {
-  stock: string;
+  stock: Ticker;
   snapshot?: Snapshot;
 }
-
-export const fetchTimeSeries = async (
-  stock: string,
-  setTimeSeries: React.Dispatch<React.SetStateAction<number[]>>,
-  setTimestamps: React.Dispatch<React.SetStateAction<string[]>>,
-) => {
-  try {
-    const response = await fetch(TIME_SERIES_ENDPOINT + stock);
-    if (!response.ok) throw new Error("Failed to fetch time series data");
-
-    const data = await response.json();
-    setTimeSeries(data.map((point: { price: number }) => point.price));
-    setTimestamps(
-      data.map((point: { timestamp: [number, number] }) => {
-        const [hour, minute] = point.timestamp;
-        return `${hour.toString().padStart(2, "0")}:${minute
-          .toString()
-          .padStart(2, "0")}`; // Format as HH:MM
-      }),
-    );
-  } catch (error) {
-    console.error("Error fetching time series data:", error);
-  }
-};
 
 function TradingCard({ stock, snapshot }: TradingCardProps) {
   const navigate = useNavigate();
 
-  const [timeSeries, setTimeSeries] = useState<number[]>([]);
-  const [timestamps, setTimestamps] = useState<string[]>([]);
-
-  useEffect(() => {
-    fetchTimeSeries(stock, setTimeSeries, setTimestamps);
-  }, [stock]);
+  const { timeSeries, timestamps } = useFetchTimeSeries(stock);
 
   return (
     <Stack
