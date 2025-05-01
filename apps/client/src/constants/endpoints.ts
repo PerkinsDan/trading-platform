fc1d7a7f11f1cc610c03d075b1b38e26baf38a67
Dan Perkins <danjonperkins@gmail.com>
2025-05-01 11:17:51 +0100

    endpoints updated

--- apps/client/src/constants/endpoints.ts
+++ apps/client/src/constants/endpoints.ts
@@ -1,6 +1,5 @@
-const ORDER_PROCESSOR_BASE_URL =
-  import.meta.env.VITE_APP_ORDER_PROCESSOR_BASE_URL ||
-  process.env.VITE_APP_ORDER_PROCESSOR_BASE_URL;
+const ORDER_PROCESSOR_BASE_URL = import.meta.env
+  .VITE_APP_ORDER_PROCESSOR_BASE_URL;
 
 export const TRADES_PENDING_ENDPOINT = `${ORDER_PROCESSOR_BASE_URL}/users/active-positions?userId=`;
 export const TRADES_HISTORY_ENDPOINT = `${ORDER_PROCESSOR_BASE_URL}/users/trade-history?userId=`;
@@ -10,8 +9,7 @@ export const USER_ACCOUNT_ENDPOINT = `${ORDER_PROCESSOR_BASE_URL}/users/account?
 export const CREATE_ORDER_ENDPOINT = `${ORDER_PROCESSOR_BASE_URL}/orders/create`;
 export const CANCEL_ORDER_ENDPOINT = `${ORDER_PROCESSOR_BASE_URL}/orders/cancel`;
 
-const MARKET_DATA_BASE_URL =
-  import.meta.env.VITE_APP_MARKET_DATA_BASE_URL ||
-  process.env.VITE_APP_MARKET_DATA_BASE_URL;
+const MARKET_DATA_BASE_URL = import.meta.env.VITE_APP_MARKET_DATA_BASE_URL;
 
-export const LATEST_SNAPSHOT_ENDPOINT = `${MARKET_DATA_BASE_URL}/latest-snapshot/`;
+export const LATEST_SNAPSHOT_ENDPOINT = `${MARKET_DATA_BASE_URL}/latest-snapshot?ticker=`;
+export const TIME_SERIES_ENDPOINT = `${MARKET_DATA_BASE_URL}/time-series?ticker=`;
