5c2401a25274033d8f2b733332c9d256bab3d47f
Victoria S <ec24599@qmul.ac.uk>
2025-05-01 15:02:53 +0100

    move usePortfolio into Portfolio.tsx

--- apps/client/src/components/Portfolio.tsx
+++ apps/client/src/components/Portfolio.tsx
@@ -1,10 +1,37 @@
 import {Stack, Typography} from "@mui/material";
 import {useAuth} from "../context/AuthContext";
-import usePortfolio from "../hooks/usePortfolio";
+import {useEffect, useState} from "react";
+import {Stock} from "../../types";
+import {USER_ACCOUNT_ENDPOINT} from "../constants/endpoints.ts";
 
 const Portfolio = () => {
     const stocks = usePortfolio();
     const currentUser = useAuth();
+    const [portfolio, setPortfolio] = useState<Stock[]>([]);
+
+    function usePortfolio(reload = 0) {
+        useEffect(() => {
+
+            if (!currentUser) {
+                console.error("User not authenticated");
+                return;
+            }
+
+            (async () => {
+                const response = await fetch(USER_ACCOUNT_ENDPOINT + currentUser.uid);
+
+                if (!response.ok) {
+                    const errorText = await response.text();
+                    throw new Error(`Failed to fetch data: ${errorText}`);
+                }
+
+                const json = await response.json();
+                setPortfolio(json.portfolio ?? []);
+            })();
+        }, [currentUser, reload]);
+
+        return portfolio;
+    }
 
     if (!currentUser) {
         console.error("User not authenticated");
