c5b9bdfdb7061983e3e07050ef03e40189cf3361
A Yezdan <yezdanasad@gmail.com>
2025-05-01 17:48:55 +0100

    undo temporary changes

--- apps/OrderProcessor/src/main/java/com/tradingplatform/orderprocessor/OrderProcessorServiceMain.java
+++ apps/OrderProcessor/src/main/java/com/tradingplatform/orderprocessor/OrderProcessorServiceMain.java
@@ -20,7 +20,6 @@ public class OrderProcessorServiceMain {
     Vertx vertx = Vertx.vertx();
     HttpServer server = vertx.createHttpServer();
 
-
     Router router = Router.router(vertx);
     router
       .route()
@@ -37,7 +36,5 @@ public class OrderProcessorServiceMain {
     server.requestHandler(router).listen(8080);
 
     if (!createConnection()) System.exit(-1);
-
-    System.out.println("Starting server...");
   }
 }
