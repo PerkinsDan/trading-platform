eb8a3fb3286232340220d6c7f00f5c2843c740aa
Dan Perkins <danjonperkins@gmail.com>
2025-05-02 02:07:03 +0100

    change config base path

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -41,7 +41,7 @@ const config: Config = {
       "classic",
       {
         docs: {
-          path: "documentation/docs",
+          path: "/",
           sidebarPath: "./sidebars.ts",
           editUrl:
             "https://github.com/PerkinsDan/trading-platform/tree/main/documentation/docs/",
